0c746e16f1b41275a0860e1124d134ad66ebdce6
Kimhyuntae9665 <Kimhyuntae9665>
2023-06-09 11:59:50 +0900

    Decoding Key를 넣으니까 Service Key에 대한 Error 가 해결

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-print("Hello World");
-print("why");
+import requests
+
+url = 'http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo'
+params ={'serviceKey' : 'bSzFPEUSrutyp2hUNcap3hiuUVNfADtwGmK3C68jTWWT5E9YrgVJK2yldsBHCy1DSZX+AA0vas6W1FZLImBtHA==', 'returnType' : 'xml', 'numOfRows' : '1', 'pageNo' : '1', 'year' : '2020', 'itemCode' : 'PM10' }
+
+response = requests.get(url, params=params)
+print(response.content)
